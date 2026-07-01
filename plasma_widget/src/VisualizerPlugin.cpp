4f31eaa836b5157a37cd493e4d168db8aa28b2bd
vorlie <vorlie0961@gmail.com>
2026-07-01 07:17:48 +0200

    .

--- plasma_widget/src/VisualizerPlugin.cpp
+++
@@ -1,7 +0,0 @@
-#include "VisualizerPlugin.hpp"
-#include "AudioEngineWrapper.hpp"
-#include <qqml.h>
-
-void VisualizerPlugin::registerTypes(const char* uri) {
-    qmlRegisterType<AudioEngineWrapper>(uri, 1, 0, "AudioEngine");
-}
